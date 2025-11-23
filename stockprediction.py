155054b9075eca1e8df99cf4cf9bb7a0f6656e28
domuhnic <domoore729@gmail.com>
2025-11-23 09:06:55 -0800

    Refactor stock prediction app for extended years
    - Earlier start date
    - Forecasting up to 10 years
    - More timeframes to view historical data
    - Relabeled graphs

--- stockprediction.py
+++ stockprediction.py
@@ -7,16 +7,16 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2015-01-01"
+START = "1970-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', 'NVDA')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
+selected_stock = st.selectbox('Select ticker', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
+years_of_prediction = st.slider('Years of prediction:', 1, 10)
+period = years_of_prediction * 365
 
 
 @st.cache_data
@@ -38,36 +38,40 @@ st.write(data.tail())
 
 # Plot raw data
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open", mode='lines'))
-fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close", mode='lines'))
+fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="Opening Price", mode='lines'))
+fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="Closing Price", mode='lines'))
 fig.update_xaxes(title_text="Date", rangeslider_visible=True, rangeselector=dict(
     buttons=list([
+        dict(count=1, label="1d", step="day"),
+        dict(count=7, label="1w", step="day"),
         dict(count=1, label="1m", step="month"),
+        dict(count=3, label="3m", step="month"),
         dict(count=6, label="6m", step="month"),
+        dict(count=1, label="1y", step="year"),
         dict(step="all")
     ])
 ))
 fig.update_yaxes(title_text="Price")
-fig.update_layout(title_text='Time Series data with Rangeslider', height=500)
+fig.update_layout(title_text='Historical Prices', height=500)
 st.plotly_chart(fig, use_container_width=True)
 
-# Predict forecast with Prophet.
+# Forecasting with Prophet.
 df_train = data[['Date','Close']]
 df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+model = Prophet()
+model.fit(df_train)
+future_prices = model.make_future_dataframe(periods=period)
+forecast = model.predict(future_prices)
 
 # Show and plot forecast
 st.subheader('Forecast data')
 st.write(forecast.tail())
     
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
+st.write(f'Forecasting for {years_of_prediction} years')
+fig1 = plot_plotly(model, forecast)
 st.plotly_chart(fig1)
 
 st.write("Forecast components")
-fig2 = m.plot_components(forecast)
+fig2 = model.plot_components(forecast)
 st.write(fig2)
